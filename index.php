5fcbea4c6201de771bc8d1ff9ddbff8c8d6553bd
태일 강 <bbq1415@gmail.com>
2025-05-15 20:40:57 +0900

    Revert "메인배너 css 수정"
    This reverts commit fcbc8833efe316f913fba24b7ff0b6aac047c8f9.

--- index.php
+++ index.php
@@ -67,7 +67,7 @@ $config = [
 <body>
     <div class="container">
         <!-- Header Section -->
-        <section class="header">
+        <header class="header">
             <div class="main-image">
                 <img src="./images/main.webp" alt="Wedding Photo">
             </div>
@@ -80,7 +80,7 @@ $config = [
                 <p><?php echo $config['wedding']['datetime']; ?></p>
                 <p><?php echo $config['wedding']['venue']; ?></p>
             </div>
-        </section>
+        </header>
         
         <!-- Poem Section -->
         <section class="poem">
