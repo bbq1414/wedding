967ff69978454ab5382c8afe7723f52ce476b55a
태일 강 <bbq1415@gmail.com>
2025-05-23 16:23:45 +0900

    html 코드만 사용하도록 소스 수정

---
+++ js/config.js
@@ -0,0 +1,36 @@
+const config = {
+    'groom' : {
+        'name' : '강태일',
+        'phone' : '010-3652-5609',
+        'father' : {'name' : '강공현', 'phone' : '010-2499-5609', 'bank' : '우리은행', 'number' : '1002-809-433276'},
+        'mother' : {'name' : '김진주', 'phone' : '010-2267-5609', 'bank' : '제일은행', 'number' : '160-20-120948'}
+    },
+    'bride' : {
+        'name' : '오다솔',
+        'phone' : '010-9948-6265',
+        'father' : {'name' : '오학만', 'phone' : '010-6822-7117', 'bank' : '신한은행', 'number' : '110-002-690637'},
+        'mother' : {'name' : '이인자', 'phone' : '010-4713-6265', 'bank' : '광주은행', 'number' : '609-122-202811'}
+    },
+    'wedding' : {
+        'date' : "2025-06-06",
+        'date_eng' : "fri. pm 1:00",
+        'datetime' : '2025년 6월 6일 금요일 오후 1시',
+        'venue' : '그린컨벤션 웨딩홀 1F',
+        'address' : '서울 도봉구 도봉1동 도봉산길 10',
+        'phone' : '02-955-2233'
+    },
+    'gallery' : {
+        'images' : [
+            './images/1.webp',
+            './images/2.webp',
+            './images/3.webp',
+            './images/4.webp',
+            './images/5.webp',
+            './images/6.webp',
+            './images/7.webp',
+            './images/8.webp',
+            './images/9.webp',
+        ]
+    },
+    'poem' : '사랑은 기다림의 미학이며<br>두 사람이 함께 걸어가는 여정입니다<br>함께 손을 잡고 걸어갈 수 있어<br>행복합니다.<br><br>저희 두 사람이<br>사랑과 믿음으로 만나<br>하나의 가정을 이루게 되었습니다<br>귀한 걸음 하시어<br>축복해 주시면 감사하겠습니다.'
+};
